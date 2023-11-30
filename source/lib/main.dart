fae2a9006a74353d805d26e4402a5e5d90d74815
vrudenia <vrudenia@asist-lab.com>
2023-11-30 11:12:18 +0300

    feat: add navigation callback `typedef NavigationCallback = Future<void> Function(BuildContext context);` when lottie is complete

--- source/lib/main.dart
+++ source/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:splash/splash.dart';
 
 void main() {
@@ -32,7 +33,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      home:  SplashScreen(doOnDone: (ctx)async{
+        Navigator.of(ctx).push(MaterialPageRoute(builder: (_)=>const SizedBox()));
+      },),
     );
   }
 }
--- source/packages/features/splash/lib/splash_screen.dart
+++ source/packages/features/splash/lib/splash_screen.dart
@@ -2,16 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:shared/shared.dart';
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+typedef NavigationCallback = Future<void> Function(BuildContext context);
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key, required this.doOnDone});
+  final NavigationCallback doOnDone;
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+    _controller.addListener(listener);
+  }
+
+  void listener() {
+    debugPrint(_controller.isCompleted.toString());
+    if (_controller.isCompleted) {
+      widget.doOnDone(context);
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(listener);
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: AppColor.violet.violet100,
       body: Center(
-        child: Lottie.asset("assets/animations/splash.json",package: "splash"),
+        child: Lottie.asset("assets/animations/splash.json",
+            controller: _controller, onLoaded: _composition, package: "splash"),
       ),
     );
+  }
+
+  void _composition(LottieComposition? composition) {
+    _controller
+      ..duration = composition?.duration
+      ..forward();
   }
 }
